edeb21dfd9566e0e27d207996750c32cbeb19bcb
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2023-04-05 10:32:15 +0800

    update predict

--- my_face_rating.py
+++ my_face_rating.py
@@ -1,13 +1,13 @@
-import MySQLdb
 from utils.config import connect_db
-from utils.predict import predict
+from predict import predict
 from os import listdir
 from os.path import join
-import tensorflow as tf
+import threading
+from keras.models import load_model
 
 
-# 生成所有臉部照片的資料表
-def prepare_face_pics(cursor):
+# 生成所有臉部照片列表
+def create_face_table(cursor):
     smile = ['原始', '經典', '微笑', '合嘴', '不開心']
     hair_style = ['原始', '殺手', '短髮']
     beard = ['原始', '文青', '山羊鬍', '剃鬍', '小山羊鬍']
@@ -22,22 +22,30 @@ def prepare_face_pics(cursor):
                                 `filename`, `rating`) \
                                 VALUES (\"{i}\", \"{j}\", \"{k}\", \"{l}\", \
                                 \"{filename}\", 0.0);'
-
                     try:
                         cursor.execute(sql_str)
-                    except Except as err:
+                    except Exception as err:
                         print(err.args)
 
 
-# 讀取製作好的所有圖片
-def evaluate_all_faces(cursor):
-    face_path = '/home/adlerhu/facial_rating/my_face/'
-    files = listdir(face_path)
-    for f in files:
-        fullpath = join(face_path, f)
-        result, points = predict(img_path=fullpath)
-        # print(f'result: {result}, points: {points}')
-        update_my_face_rating(cursor, points=points, filename=f)
+# 將一個list按餘數切分成多個子list
+def split_list_by_mod(lst, mod):
+    return [lst[i::mod] for i in range(mod)]
+
+
+# 讀取製作好的所有圖片，並按線程數切成幾個list
+def prepare_face_lists(myface_dir, threading_number):
+    return split_list_by_mod(listdir(myface_dir), threading_number)
+
+
+def evaluate_faces(myface_dir, face_list, model):    
+    connection, cursor = connect_db()
+    for filename in face_list:
+        fullpath = join(myface_dir, filename)
+        result, points = predict(img_path=fullpath, model=model)
+        update_my_face_rating(cursor, points=points, filename=filename)
+    cursor.close()
+    connection.close()
 
 
 # 將模型預測的結果寫入DB
@@ -45,20 +53,40 @@ def update_my_face_rating(cursor, points, filename):
     sql_str = f'UPDATE `my_face_rating` SET `rating` = {points} WHERE `filename` = \"{filename}\";'
     try:
         cursor.execute(sql_str)
-    except Except as err:
+    except Exception as err:
         print(err.args)
+    
 
+def main(myface_dir, threading_number):
 
-def main():
+    model = load_model('model/26-0.12.h5')
 
-    db, cursor = connect_db()    
-    prepare_face_pics(cursor)
-    evaluate_all_faces(cursor)
-
+    # 建立我所有照片的表
+    connection, cursor = connect_db()
+    create_face_table(cursor)
     cursor.close()
-    db.close()
-    print('done')
+    connection.close()
+
+    face_lists = prepare_face_lists(myface_dir, threading_number)
+
+    # 創建多線程寫入數據
+    threads = []
+    for i in range(threading_number):
+        t = threading.Thread(target=evaluate_faces, args=(myface_dir, face_lists[i], model))
+        threads.append(t)
+    
+    # 啟動線程
+    for t in threads:
+        t.start()
+
+    # 等待線程完成
+    for t in threads:
+        t.join()
+
+    print('Done')
 
 
 if __name__ == '__main__':
-    main()
+    myface_dir = '/home/adlerhu/facial_rating/my_face/'
+    # threading_number: 預計跑的線程數
+    main(myface_dir, threading_number=8)
--- predict.py
+++ predict.py
@@ -1,24 +1,20 @@
-import keras
-import matplotlib.pyplot as plt
 from PIL import Image
+from keras.utils import img_to_array
 
- 
+
 def predict(model, img_path):
-    plt.rcParams['font.size'] = 9
-    plt.rcParams['figure.figsize'] = (9, 9)    
 
     img = Image.open(img_path)
-    plt.imshow(img)
     img_width, img_height, channels = 350, 350, 3
+    # 傳入的圖片要縮放到跟資料集圖片一樣的大小
     img = img.resize([img_width, img_height])
     
-    test_x = keras.utils.img_to_array(img).reshape(img_height, img_width, channels)
-    test_x = test_x / 255
-    test_x = test_x.reshape((1,) + test_x.shape)
+    test_img = img_to_array(img).reshape(img_height, img_width, channels)
+    test_img = test_img / 255
+    test_img = test_img.reshape((1,) + test_img.shape)
 
-    # prediction = model.predict(test_x)
-    prediction = model(test_x, training=False)
+    prediction = model.predict(test_img)
 
     points = round(float(prediction[0][0]), 3)
-    result = '美' if points > 3 else '醜'
+    result = '好看' if points > 3 else '普通'
     return result, points
