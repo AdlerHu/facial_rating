ace69036a1e866ea796c11adf4314a9dce26a9e3
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2023-04-07 08:58:49 +0800

    porgram add note

--- predict.py
+++ predict.py
@@ -4,17 +4,21 @@ from keras.utils import img_to_array
 
 def predict(model, img_path):
 
+    # 使用 pillow 讀取圖片
     img = Image.open(img_path)
-    img_width, img_height, channels = 350, 350, 3
+
     # 傳入的圖片要縮放到跟資料集圖片一樣的大小
+    img_width, img_height, channels = 350, 350, 3
     img = img.resize([img_width, img_height])
-    
+
+    # 將縮放過的圖片轉換成 ndarray 
     test_img = img_to_array(img).reshape(img_height, img_width, channels)
     test_img = test_img / 255
     test_img = test_img.reshape((1,) + test_img.shape)
 
     prediction = model.predict(test_img)
 
+    # 滿分5分，得分超過 3 就認定為好看
     points = round(float(prediction[0][0]), 3)
     result = '好看' if points > 3 else '普通'
     return result, points
--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 from keras.models import load_model
-from flask import Flask, render_template, request, redirect, url_for, make_response,jsonify
+from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os
 import cv2
@@ -7,45 +7,46 @@ import time
 from datetime import timedelta
 from predict import predict
 
- 
-# 設定允許的圖片格式
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
- 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
- 
+
 app = Flask(__name__)
 
 # 緩存過期時間
 app.send_file_max_age_default = timedelta(seconds=5)
+# 啟動網頁應用程式時就先載入預測模型
 model = load_model('model/26-0.12.h5')
 
-# route 
+# 設定允許的圖片格式
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+# route
 @app.route('/', methods=['POST', 'GET'])
 def upload():
     if request.method == 'POST':
         f = request.files['file']
- 
-        if not (f and allowed_file(f.filename)): 
+
+        if not (f and allowed_file(f.filename)):
             return jsonify({"error": 1001, "msg": "圖片格式僅限 png、PNG、jpg、JPG、bmp"})
- 
+
         user_input = request.form.get("name")
- 
+
         # 當前文件所在路徑
         basepath = os.path.dirname(__file__)
-        upload_path = os.path.join(basepath, 'static/images', secure_filename(f.filename))        
+        upload_path = os.path.join(basepath, 'static/images', secure_filename(f.filename))
         f.save(upload_path)
- 
+
         # 使用轉換opencv轉換圖片格式、名稱
         img = cv2.imread(upload_path)
         cv2.imwrite(os.path.join(basepath, 'static/images', 'test.jpg'), img)
 
         result, point = predict(model, img_path=basepath+'/static/images/test.jpg')
 
-        return render_template('upload_ok.html',result=result, point=point, val1=time.time())
- 
+        return render_template('upload_ok.html', result=result, point=point, val1=time.time())
+
     return render_template('upload.html')
- 
- 
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8987, debug=True)
+    app.run(host='0.0.0.0', port=8987)
